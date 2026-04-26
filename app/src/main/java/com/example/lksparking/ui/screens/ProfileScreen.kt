19ea7dcadc326f9123b1ae7a88ffee93294fc42d
Mikel Martinez <mmartinez294@ikasle.ehu.eus>
2026-04-26 22:07:55 +0200

    Los cimientos de la app estan creados, en este commit entra el perfil, creo que me ha quedado tal y como lo queria asique de momento muy contento

--- app/src/main/java/com/example/lksparking/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/lksparking/ui/screens/ProfileScreen.kt
@@ -1,18 +1,112 @@
 package com.example.lksparking.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DirectionsCar
+import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lksparking.model.VehicleType
+import com.example.lksparking.ui.components.ProfileHeaderSection
+import com.example.lksparking.ui.components.VehicleCard
+import com.example.lksparking.ui.theme.LksOrange
 
 @Composable
 fun ProfileScreen(
-
+    onAddVehicleClick: () -> Unit
 ){
+    //DATOS DE PRUEBA
+    val userName = "1Myke"
+    val userRole = "Senior Operations Manager"
+    val userEmail = "mikel@lksnext.com"
 
+    Scaffold(
+        // SOLO para el boton flotante naranja
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddVehicleClick,
+                containerColor = LksOrange,
+                contentColor = Color.White,
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .background(Color.White) //????HACE FALTA
+                .verticalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+            ProfileHeaderSection(userName, userRole, userEmail)
+
+            Divider(modifier = Modifier.padding(vertical = 24.dp), thickness = 1.dp, color = Color(0xFFEEEEEE))
+
+            Row(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    Icons.Default.DirectionsCar,
+                    contentDescription = null,
+                    tint = LksOrange,
+                    modifier = Modifier.size(20.dp)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(
+                    text = "My Vehicles",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+
+            Column(
+                modifier = Modifier.padding(24.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                VehicleCard(name = "My Car", plate = "1234 ABC", type = VehicleType.STANDARD)
+                VehicleCard(name = "Vespa", plate= "5678 XYZ", type = VehicleType.MOTORCYCLE)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ProfileScreenPreview() {
     ProfileScreen(
+        onAddVehicleClick = {}
     )
 }
